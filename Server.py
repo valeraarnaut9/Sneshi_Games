e4b2fbb6b529e40d70467117f4a238c13f042516
valeraarnaut9 <valeraarnaut35@gmail.com>
2026-04-16 01:15:22 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def show_one():
-    return '1'
+    return "1"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
