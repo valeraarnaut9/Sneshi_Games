12e1422f6c5e6e81c323f60dc03e496fe884fbed
valeraarnaut9 <valeraarnaut35@gmail.com>
2026-04-16 01:58:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+API_KEY = "afg68h5gi57ghyy4479qr36llbzxc468"
+
 @app.route("/")
 def home():
-    return "1"
+    key = request.args.get("key")
+
+    if key == API_KEY:
+        return "1"
+    else:
+        return "NO ACCESS"
+
+if __name__ == "__main__":
+    app.run()
