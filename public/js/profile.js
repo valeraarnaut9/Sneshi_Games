e8a2376d77a514b3bdb2b65f4929def10fd4572c
valeraarnaut9 <valeraarnaut35@gmail.com>
2026-07-21 00:21:48 +0300

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,1 +1,151 @@
+const openButton =
+document.getElementById("openEditProfile");
 
+
+const closeButton =
+document.getElementById("closeEditProfile");
+
+
+const overlay =
+document.getElementById("profileEditOverlay");
+
+
+
+if(openButton){
+
+
+    openButton.onclick = ()=>{
+
+        overlay.classList.add("show");
+
+    };
+
+
+}
+
+
+
+
+
+if(closeButton){
+
+
+    closeButton.onclick = ()=>{
+
+        overlay.classList.remove("show");
+
+    };
+
+
+}
+
+
+
+
+
+if(overlay){
+
+
+    overlay.onclick = (e)=>{
+
+
+        if(e.target === overlay){
+
+            overlay.classList.remove("show");
+
+        }
+
+
+    };
+
+
+}
+
+
+
+
+
+const form =
+document.getElementById("editProfileForm");
+
+
+
+if(form){
+
+
+form.addEventListener("submit", async(e)=>{
+
+
+    e.preventDefault();
+
+
+
+    const display_name =
+    document.getElementById("displayNameInput").value;
+
+
+
+    const bio =
+    document.getElementById("bioInput").value;
+
+
+
+
+
+    const response =
+    await fetch(
+        "/api/profile/edit",
+        {
+
+            method:"POST",
+
+            headers:{
+
+                "Content-Type":
+                "application/json"
+
+            },
+
+            body:JSON.stringify({
+
+                display_name,
+
+                bio
+
+            })
+
+        }
+    );
+
+
+
+
+
+    const data =
+    await response.json();
+
+
+
+
+
+    if(data.success){
+
+
+        location.reload();
+
+
+    }
+    else{
+
+
+        alert(data.message);
+
+
+    }
+
+
+
+});
+
+
+}
